ae923815897bcff887b09792df36ea4da6c862c2
William Chen <williamchen@10-19-136-25.dynapool.wireless.nyu.edu>
2019-04-25 13:13:58 -0400

    add view change

--- PlantManiaGame/GameViewController.swift
+++ PlantManiaGame/GameViewController.swift
@@ -8,20 +8,85 @@
 
 import UIKit
 
+protocol GameProtocol{
+    
+}
+
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var incubator_view: UIView!
+    
+
+    @IBOutlet weak var garden_view: UIView!
+    @IBOutlet weak var store_view: UIView!
     
     @IBOutlet weak var current_plant: UIImageView!
-    
     @IBOutlet weak var plant_age: UILabel!
+    
+    var allPlants = Array<Plant>()
+    var indexOfPlant: Int  = 0
+    
+    @IBOutlet weak var maturity_bar: UIProgressView!
+
+    @IBAction func switch_segment(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            garden_view.isHidden = true
+            store_view.isHidden = true
+        case 1:
+            garden_view.isHidden = false
+            store_view.isHidden = true
+        case 2:
+            garden_view.isHidden = true
+            store_view.isHidden = false
+        default:
+            break
+        }
+    }
+    
+    
+    @IBAction func next_button(_ sender: Any) {
+        if(indexOfPlant < allPlants.count - 1 ){
+            indexOfPlant += 1
+            current_plant.image = UIImage( named: allPlants[indexOfPlant].third_stage)
+            plant_age.text = String(allPlants[indexOfPlant].age) + " Days Old"
+            maturity_bar.progress = Float(allPlants[indexOfPlant].age) * 0.1
+        }
+    }
+    
+    @IBAction func back_button(_ sender: Any) {
+        if(indexOfPlant != 0){
+            indexOfPlant -= 1
+            current_plant.image = UIImage( named: allPlants[indexOfPlant].third_stage)
+            plant_age.text = String(allPlants[indexOfPlant].age) + " Days Old"
+            maturity_bar.progress = Float(allPlants[indexOfPlant].age) * 0.1
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        current_plant.image = UIImage(named: Cactus.third_stage)
-        plant_age.text = String(Cactus.age) + " Days Old"
-
+        allPlants.append(Rose)
+        allPlants.append(Sunflower)
+        allPlants.append(Lilac)
+        allPlants.append(Cactus)
+        
+        allPlants[0].age = 2
+        allPlants[1].age = 5
+        allPlants[2].age = 8
+        allPlants[3].age = 10
+        
+        current_plant.image = UIImage(named: allPlants[indexOfPlant].third_stage)
+        plant_age.text = String(allPlants[indexOfPlant].age) + " Days Old"
+        
+        maturity_bar.progressTintColor = .blue
+        maturity_bar.transform = maturity_bar.transform.scaledBy(x: 1, y: 8)
+        maturity_bar.progress = Float(allPlants[indexOfPlant].age) * 0.1
     }
 
+}
 
+extension GameViewController : GameProtocol{
+    
 }
--- PlantManiaGame/PlantClasses.swift
+++ PlantManiaGame/PlantClasses.swift
@@ -11,16 +11,18 @@ class Plant {
     var plant_name: String!
     var max_water: Int!
     var current_water: Int!
+    var seed: String!
     var first_stage: String!
     var second_stage: String!
     var third_stage: String!
     var age: Int!
     
-    init(plant_name: String, max_water: Int, current_water: Int, first_stage: String, second_stage: String, third_stage: String, age: Int){
+    init(plant_name: String, max_water: Int, current_water: Int, seed: String, first_stage: String, second_stage: String, third_stage: String, age: Int){
         
         self.plant_name = plant_name
         self.max_water = max_water
         self.current_water = current_water
+        self.seed = seed
         self.first_stage = first_stage
         self.second_stage = second_stage
         self.third_stage = third_stage
@@ -29,10 +31,10 @@ class Plant {
     
 }
 
-let Cactus = Plant(plant_name: "Cactus", max_water: 10, current_water: 10, first_stage: "pot", second_stage: "sprout            ", third_stage: "cactus", age: 0)
+let Cactus = Plant(plant_name: "Cactus", max_water: 10, current_water: 10, seed: "sunflower-seed-clipart-1", first_stage: "pot", second_stage: "sprout", third_stage: "cactus", age: 0)
 
-let Sunflower = Plant(plant_name: "Sunflower", max_water: 10, current_water: 10, first_stage: "pot", second_stage: "sprout", third_stage: "sunflower", age: 0)
+let Sunflower = Plant(plant_name: "Sunflower", max_water: 10, current_water: 10, seed: "sunflower-seed-clipart-1", first_stage: "pot", second_stage: "sprout", third_stage: "sunflower", age: 0)
 
-let Lilac = Plant(plant_name: "Lilac", max_water: 10, current_water: 10, first_stage: "pot", second_stage: "sprout", third_stage: "lilac", age: 0)
+let Lilac = Plant(plant_name: "Lilac", max_water: 10, current_water: 10, seed: "sunflower-seed-clipart-1", first_stage: "pot", second_stage: "sprout", third_stage: "lilac", age: 0)
 
-let Rose = Plant(plant_name: "Rose", max_water: 10, current_water: 10, first_stage: "pot", second_stage: "sprout", third_stage: "rose", age: 0)
+let Rose = Plant(plant_name: "Rose", max_water: 10, current_water: 10, seed: "sunflower-seed-clipart-1", first_stage: "pot", second_stage: "sprout", third_stage: "rose", age: 0)
