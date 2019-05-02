6f8dee203c8e2ba15c00527347079d5e7a1eb108
William Chen <williamchen@Williams-MacBook-Pro-2.local>
2019-05-02 03:27:52 -0400

    add garden view controller

--- PlantManiaGame/GameViewController.swift
+++ PlantManiaGame/GameViewController.swift
@@ -14,35 +14,36 @@ protocol GameProtocol{
 
 class GameViewController: UIViewController {
     
-    @IBOutlet weak var incubator_view: UIView!
+    //var allPlants = GlobalVariables.allPlants
+    var allPlants = Array<Plant>()
+    var indexOfPlant: Int  = 0
+    var gardenPlants = Array<Plant>()
     
-
-    @IBOutlet weak var garden_view: UIView!
-    @IBOutlet weak var store_view: UIView!
+    @IBOutlet weak var incubator_view: UIView!
+//    @IBOutlet weak var garden_view: UIView!
+//    @IBOutlet weak var store_view: UIView!
     
     @IBOutlet weak var current_plant: UIImageView!
     @IBOutlet weak var plant_age: UILabel!
     
-    var allPlants = Array<Plant>()
-    var indexOfPlant: Int  = 0
     
     @IBOutlet weak var maturity_bar: UIProgressView!
-
-    @IBAction func switch_segment(_ sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            garden_view.isHidden = true
-            store_view.isHidden = true
-        case 1:
-            garden_view.isHidden = false
-            store_view.isHidden = true
-        case 2:
-            garden_view.isHidden = true
-            store_view.isHidden = false
-        default:
-            break
-        }
-    }
+    
+//    @IBAction func switch_segment(_ sender: UISegmentedControl) {
+//        switch sender.selectedSegmentIndex {
+//        case 0:
+//            garden_view.isHidden = true
+//            store_view.isHidden = true
+//        case 1:
+//            garden_view.isHidden = false
+//            store_view.isHidden = true
+//        case 2:
+//            garden_view.isHidden = true
+//            store_view.isHidden = false
+//        default:
+//            break
+//        }
+//    }
     
     
     @IBAction func next_button(_ sender: Any) {
@@ -63,9 +64,33 @@ class GameViewController: UIViewController {
         }
     }
     
+    @IBAction func add_garden(_ sender: Any) {
+        
+        let addGardenPopUpVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopUpID")as! IncubatorPopUpViewController
+        self.addChild(addGardenPopUpVC)
+        addGardenPopUpVC.view.frame = self.view.frame
+        self.view.addSubview(addGardenPopUpVC.view)
+        addGardenPopUpVC.didMove(toParent: self)
+        
+        gardenPlants.append(allPlants[indexOfPlant])
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        garden_view.isHidden = true
+//        store_view.isHidden = true
+        
+//        GlobalVariables.allPlants.append(Rose)
+//        GlobalVariables.allPlants.append(Sunflower)
+//        GlobalVariables.allPlants.append(Lilac)
+//        GlobalVariables.allPlants.append(Cactus)
+//
+//        GlobalVariables.allPlants[0].age = 2
+//        GlobalVariables.allPlants[1].age = 5
+//        GlobalVariables.allPlants[2].age = 8
+//        GlobalVariables.allPlants[3].age = 10
         
         allPlants.append(Rose)
         allPlants.append(Sunflower)
@@ -84,6 +109,16 @@ class GameViewController: UIViewController {
         maturity_bar.transform = maturity_bar.transform.scaledBy(x: 1, y: 8)
         maturity_bar.progress = Float(allPlants[indexOfPlant].age) * 0.1
     }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let gardenViewController = segue.destination as? GardenViewController
+            else {
+                return
+            }
+        gardenViewController.garden_plants = gardenPlants
+    }
+    @IBAction func cancel(_ unwindSegue: UIStoryboardSegue) {}
 
 }
 
---
+++ PlantManiaGame/IncubatorPopUpViewController.swift
@@ -0,0 +1,58 @@
+//
+//  IncubatorPopUpViewController.swift
+//  PlantManiaGame
+//
+//  Created by William Chen on 5/2/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class IncubatorPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        self.showAnimate()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func close(_ sender: Any) {
+        self.removeAnimate()
+    }
+    
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
