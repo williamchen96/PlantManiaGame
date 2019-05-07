e749bc7b94f27fdef3e22aa0e1a2e5b08c1de34f
Emily Chang <efc269@nyu.edu>
2019-05-07 15:34:22 -0400

    collection view

--- PlantManiaGame/GardenViewController.swift
+++ PlantManiaGame/GardenViewController.swift
@@ -14,34 +14,40 @@ class GardenViewController: UIViewController, UICollectionViewDelegate, UICollec
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return garden_plants.count
     }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "plantCell", for: indexPath)
-        cell.backgroundColor = UIColor.white
-        //cell.imageView.image = UIImage(named: "rose.png")
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "plantCell", for: indexPath) as! PlantCollectionViewCell
+        current_index += 1
+        //plant_image.image = UIImage(named: garden_plants[indexPath.row].third_stage)
+        cell.imageView.image = UIImage(named: garden_plants[indexPath.row].third_stage)
+        /*
+        if(current_index < garden_plants.count - 1 ){
+            current_index += 1
+            plant_image.image = UIImage(named: garden_plants[current_index].third_stage)
+            cell.imageView.image = plant_image.image
+        }
+        */
+        //cell.imageView.image = UIImage(named: "rose")
         return cell
-        
-        //if(current_index < garden_plants.count - 1 ){
-        //    current_index += 1
-        //    cell.imageView.image  = plant_image.image = UIImage( named: garden_plants[current_index].third_stage)
-        //}
     }
     
 
     var garden_plants = Array<Plant>()
     var current_index = 0
 
-    @IBOutlet weak var plant_name: UILabel!
-    @IBOutlet weak var plant_image: UIImageView!
-    @IBOutlet weak var plant_number: UILabel!
+    //@IBOutlet weak var plant_name: UILabel!
+    //@IBOutlet weak var plant_image: UIImageView!
+    //@IBOutlet weak var plant_number: UILabel!
     
     //Collection View
     @IBOutlet weak var collectionView: UICollectionView!
     
+    /*
     @IBAction func next_plants(_ sender: Any) {
         if(current_index < garden_plants.count - 1 ){
             current_index += 1
             plant_name.text = garden_plants[current_index].plant_name
-            plant_image.image = UIImage( named: garden_plants[current_index].third_stage)
+            plant_image.image = UIImage( named: garden_plants[current_index]s.third_stage)
             plant_number.text = "Plant Number " + String(current_index+1)
         }
     }
@@ -54,6 +60,7 @@ class GardenViewController: UIViewController, UICollectionViewDelegate, UICollec
             plant_number.text = "Plant Number " + String(current_index+1)
         }
     }
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,15 +69,19 @@ class GardenViewController: UIViewController, UICollectionViewDelegate, UICollec
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        /*
         if(garden_plants.count == 0){
             plant_name.text = "No plants in garden!"
             plant_number.text = "Go add more plants!"
         }
+        */
+        /*
         else{
             plant_name.text = garden_plants[0].plant_name
             plant_image.image = UIImage(named: garden_plants[0].third_stage)
             plant_number.text = "Plant Number " + String(current_index+1)
         }
+        */
         // Do any additional setup after loading the view.
     }
     
---
+++ PlantManiaGame/PlantCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PlantCollectionViewCell.swift
+//  PlantManiaGame
+//
+//  Created by Emily Chang on 5/7/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class PlantCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
